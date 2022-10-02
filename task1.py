4cde68846627df1ccb50fa97d347d59a215f0086
marvari <113026614+marvari@users.noreply.github.com>
2022-10-02 23:13:55 +0300

    my first programm

---
+++ task1.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math  # Подключение модуля "math"
+import numpy  # Подключение модуля "numpy"
+import matplotlib.pyplot as mpp  # Подключение модуля "matplotlib", для данного модуля задается сокращение "mpp"
+
+# Эта программа рисует график функции, заданной выражением ниже
+
+if __name__ == '__main__':  # Проверка на то, запущен ли файл напрямую
+    arguments = numpy.arange(0, 200, 0.1)  # Задаем значения по оси ОХ из данного диапазона с конкретным интервалом
+    mpp.plot(arguments, [math.sin(a) * math.sin(a / 20.0) for a in
+             arguments])  # Значения на оси OY получаются по следующей формуле
+    mpp.show()  # Вывод графика

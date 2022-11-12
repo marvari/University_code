8082409b205db82abe4c94f9ec70af4975cefdf0
marvari <113026614+marvari@users.noreply.github.com>
2022-11-12 22:15:59 +0300

    Euclidean_algorithm

---
+++ task5.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+(a, b) = (int(input("Введите числа через 'enter'")), int(input()))
+
+
+def my_gcd(a, b):
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
+print ('наш нод:', my_gcd(a, b), 'библиотечный нод:', math.gcd(a, b))

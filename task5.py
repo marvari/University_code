4bfd0c5d7d299ddeed6af089bb743815db40fc25
marvari <113026614+marvari@users.noreply.github.com>
2022-11-13 08:29:10 +0300

    Euclidean_algorithm

---
+++ task5.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+(a, b) = (int(input("Ищем НОД двух чисел двумя  способами: с помощью библиотеки math и собственной функции. Введите первое число ")), int(input("Введите второе число ")))
+
+
+def my_gcd(a, b):
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
+print ('наш нод:', my_gcd(a, b), 'библиотечный нод:', math.gcd(a, b))
